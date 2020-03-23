6989f8ec49c4e6ad2aa7df585d18a33a7741186d
EdenChien <eden851104@gmail.com>
2020-03-23 20:13:56 +0800

    Change the njobs from 20 to 10

--- hw1/reproduce.sh
+++ hw1/reproduce.sh
@@ -3,4 +3,4 @@ output_file=$2
 wget -O rnnt_latest.pth https://www.dropbox.com/s/idq7abyzkj6ev90/rnnt_latest.pth?dl=1
 sed -i "s#DATA_PATH_TO_BE_CHANGED#${data_root}#g" config/dlhlp/asr_dlhlp_layer5_w_pretrained_rnnt.yaml
 sed -i "s#OUTPUT_FILE_TO_BE_CHANGED#${output_file}#g" config/dlhlp/decode_rnnt_beam.yaml
-python main.py --config config/dlhlp/decode_rnnt_beam.yaml --test --njobs 20
+python main.py --config config/dlhlp/decode_rnnt_beam.yaml --test --njobs 10
